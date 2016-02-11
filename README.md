ce25713dd0f9db80fd9e51cf944bd29667ae117c
Nonemoticoner <nonemoticoner@gmail.com>
2016-02-11 19:15:20 +0100

    First release
    Tested and worked.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # hotslogs
 A Node module for HOTS (Heroes of the Storm) Logs.
+
+#### Installation
+```bash
+$ npm install hotslogs
+```
+
+#### How to use
+```javascript
+var hl = require('hotslogs');
+
+hl.getID('machina#2809', 'EU', function (id) {
+	console.log(id);		// 5358817
+});
+
+hl.getData(5358817, function (player) {
+	console.log(player.name);	// machina
+});
+
+```
+
+#### Documentation
+**getID(battletag, region, callback);**
+
+Arguments:
+* *battletag* (string)
+* *region* (string): shortcut e.g. 'EU', 'US', 'KR', 'CN'
+* *callback* (function): with integer argument
+
+The above function allows you to acquire an unique ID on HOTS Logsfrom which you could generate such link: (http://www.hotslogs.com/Player/Profile?PlayerID=5358817)
+
+**getData(id, callback);**
+
+Arguments:
+* *id* (integer): can be acquired with getID(...)
+* *callback* (function): with object argument
+
+The above function allows you to acquire such kind of object:
+```javascript
+{
+	id: 5358817,
+	name: "machina",
+	rankings: {
+		quickMatch: {
+			MMR: 2087,
+			rank: 13222
+		},
+		heroLeague: {
+			MMR: 1688,
+			rank: 11992
+		},
+		teamLeague: {
+			MMR: null,	// because player never played Team League
+			rank: null
+		}
+	}
+}
+```
+
+#### Remark
+It is unofficial API support for (http://www.hotslogs.com).
+
+#### License
+MIT
--- index.js
+++ index.js
@@ -5,8 +5,103 @@
  * Copyright (c) 2016 Nonemoticoner
  * Licensed under the MIT license.
  */
+var request = require('request');
 
 module.exports = {
-	
+
+	// get unique ID (integer) that is assigned to player in HOTS Logs
+	getID: function (battletag, region, callback) {
+		var name = battletag.replace('#', '_').trim(),
+			region_no = 0;
+
+		switch(region){
+			case "US":
+				region_no = 1;
+				break;
+			case "EU":
+				region_no = 2;
+				break;
+			case "KR":
+				region_no = 3;
+				break;
+			case "CN":
+				region_no = 5;
+				break;
+		}
+
+		var address = 'https://www.hotslogs.com/API/Players/' + region_no.toString() + '/' + name;
+
+		request(address, function (error, response, body) {
+			if(!error && response.statusCode == 200){
+				var data = JSON.parse(body);
+
+				if(data != null && typeof data.PlayerID == "number"){
+					callback(data.PlayerID);
+				}
+				else{
+					callback(null);
+				}
+			}
+			else{
+				throw error;
+			}
+		});
+	},
+
+	// get basic data about player
+	getData: function (id, callback) {
+		var address = 'https://www.hotslogs.com/API/Players/' + id.toString();
+
+		request(address, function (error, response, body) {
+			if(!error && response.statusCode == 200){
+				var data = JSON.parse(body);
+
+				if(data != null){
+					var player = {};
+
+					player.id = data.PlayerID;
+					player.name = data.Name;
+					player.rankings = {};
+					player.rankings.quickMatch = {
+						MMR: null,
+						rank: null
+					};
+					player.rankings.heroLeague = {
+						MMR: null,
+						rank: null
+					};
+					player.rankings.teamLeague = {
+						MMR: null,
+						rank: null
+					};
+
+					for (var i = data.LeaderboardRankings.length - 1; i >= 0; i--) {
+						switch(data.LeaderboardRankings[i].GameMode){
+							case "QuickMatch":
+								player.rankings.quickMatch.MMR = data.LeaderboardRankings[i].CurrentMMR;
+								player.rankings.quickMatch.rank = data.LeaderboardRankings[i].LeagueRank;
+								break;
+							case "HeroLeague":
+								player.rankings.heroLeague.MMR = data.LeaderboardRankings[i].CurrentMMR;
+								player.rankings.heroLeague.rank = data.LeaderboardRankings[i].LeagueRank;
+								break;
+							case "TeamLeague":
+								player.rankings.teamLeague.MMR = data.LeaderboardRankings[i].CurrentMMR;
+								player.rankings.teamLeague.rank = data.LeaderboardRankings[i].LeagueRank;
+								break;
+						}
+					}
+
+					callback(player);
+				}
+				else{
+					callback(null);
+				}
+			}
+			else{
+				throw error;
+			}
+		});
+	}
 	
 };
